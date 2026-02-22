3ab2a3f47f68c4e91c3042eac646f1fd1649b0dc
sagar <sdev77577@gmail.com>
2026-02-22 18:40:09 +0530

    dgasdf

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import ExpenseCard from "./components";
 
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <ExpenseCard
-        title="Grocery Shopping"
-        amount={1200}
-        category="Food"
-        date="22 Feb 2026"
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
