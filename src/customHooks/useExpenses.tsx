0b08ef74275a3aad5e61bad08940e92630137f8f
sachin <sdev77577@gmail.com>
2026-02-24 21:40:46 +0530

    asfas

---
+++ src/customHooks/useExpenses.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import { ENewExpense } from "../types/types";
+
+const useExpenses=()=>{
+    const [expenses,setExpenses]=useState<ENewExpense[]>([]);
+    const addExpenses=(expenseData :ENewExpense)=>{
+        const newExpense={
+            ...expenseData,
+            date:new Date().toISOString().split("T")[0]
+        };
+        return setExpenses(prevExpenses=>[newExpense,...prevExpenses]);
+
+    }
+    const removeExpense=(id:number)=>{
+     return   setExpenses(prevExpenses=>prevExpenses.filter((expense)=>expense.id!==id))
+    }
+    const getTotalAmount=()=>{
+const totalExpenseAmount=expenses.reduce((total,expense)=>total+(expense.amount ?? 0) ,0);
+return totalExpenseAmount;
+    }
+    const getExpenseByCategory=(category:string)=>{
+if(!category || category ==="All") return expenses;
+return expenses.filter((expense)=>expense.category===category);
+
+    }
+    return {    expenses,
+     addExpenses,
+     removeExpense,
+     getExpenseByCategory,
+     getTotalAmount
+ }   ;
+}
